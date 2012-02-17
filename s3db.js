85f38b5790176e07ea09c7f97e08e45e76569fe6
David Robbins <robbins.david@gmail.com>
2012-02-16 21:26:25 -0600

    WIP tests for deployment making

--- s3db.js
+++ s3db.js
@@ -7,6 +7,16 @@
   //Version
   S3DB.Version = "0.0.1";
 
+  //Deployments
+  S3DB.Deployment = function Deployment(){
+    try{
+      this.store = new rdfstore.Store();
+    }
+    catch(e){
+      throw "rdf_store required to create deployments";
+    }
+  };
+
   //Put S3DB on the root object
   root.S3DB = root.S3DB || S3DB;
 }).call(this);
--- spec/s3db.js.spec.js
+++ spec/s3db.js.spec.js
@@ -1,6 +1,46 @@
 describe("S3DB.js", function(){
 
+  beforeEach(function(){
+    var toType = function(obj) {
+          return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
+        },
+        custom_matchers = {};
+
+    custom_matchers.toBeA = function toBeA(expected_type){
+      return this.actual === expected_type;
+    };
+
+    custom_matchers.toBeAFunction = function toBeAFunction(){
+      return this.actual === 'function';
+    };
+
+    custom_matchers.toBeAObject = function toBeAObject(){
+      return this.actual === 'object';
+    };
+
+    this.addMatchers(custom_matchers);
+  });
+
   it("should put S3DB as an object on the root object", function(){
-    expect(typeof S3DB).toBe('object');
+    expect(toType(S3DB)).toBe('object');
+  });
+
+  describe("Deployments", function(){
+
+    it("should provide an S3DB deployment constructor", function(){
+      expect(toType(S3DB.Deployment)).toBe('function');
+    });
+
+    it("should throw an exception when trying to create a deployment without an rdf_store", function(){
+      var temp = rdfstore;
+      delete rdfstore;
+      expect(S3DB.Deployment).toThrow("rdf_store required to create deployments");
+      rdfstore = temp;
+    });
+
+    it("should expose a store property", function(){
+      var deployment = new S3DB.Deployment();
+      expect(deployment.store).to
+    });
   });
 });
