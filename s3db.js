4813460fa471166ef9593eb249b7b26063db769d
David Robbins <robbins.david@gmail.com>
2012-02-16 20:19:10 -0600

    Added s3db.js and it's associated spec

---
+++ s3db.js
@@ -0,0 +1,12 @@
+(function(){
+  "use strict";
+
+  var root = this,
+      S3DB = {};
+
+  //Version
+  S3DB.Version = "0.0.1";
+
+  //Put S3DB on the root object
+  root.S3DB = root.S3DB || S3DB;
+}).call(this);
--- spec/s3db.js.spec.js
+++ spec/s3db.js.spec.js
@@ -1,6 +1,6 @@
 describe("S3DB.js", function(){
 
-  it("should be true", function(){
-    expect(false).toBeTruthy();
+  it("should put S3DB as an object on the root object", function(){
+    expect(typeof S3DB).toBe('object');
   });
 });
